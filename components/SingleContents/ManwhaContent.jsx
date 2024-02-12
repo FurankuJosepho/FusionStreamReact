2464de389960dbe5f879fe12e7c9742b2b20a5b9
FurankuJosepho <frank21garcia29@gmail.com>
2024-02-12 23:17:40 +0800

    dones

--- components/SingleContents/ManwhaContent.jsx
+++ components/SingleContents/ManwhaContent.jsx
@@ -1,18 +1,28 @@
 import { useParams } from "react-router-dom";
 import Manwha from "../../json/Manwha.json";
+import "../../scss/SingleContents.scss";
 
 const ManwhaContent = () => {
   const { title } = useParams();
   const ManwhaDetail = Manwha.find((manwha) => manwha.title === title);
-  const { ratings, image, episodes } = ManwhaDetail;
+  const { ratings, image, episodes, description } = ManwhaDetail;
 
   return (
-    <div>
-      <h1>{title}</h1>
-      <p>Rating: {ratings}</p>
-      <p>{episodes}</p>
-      <img src={image} alt={title} />
-    </div>
+    <section className="SingleContents">
+      <div className="Container">
+        <div className="Left">
+          <img src={image} alt={title} />
+        </div>
+        <div className="Right">
+          <div className="Details">
+            <h1>{title}</h1>
+            <p>{episodes}</p>
+            <p>Rating: {ratings}</p>
+            <p>Plot Sumarry: <span>{description}</span></p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
